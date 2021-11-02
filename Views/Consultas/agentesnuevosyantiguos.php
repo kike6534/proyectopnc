afa616150b280912c1bdf946016e5d1f77b343d6
Kelvin252525 <vc18061@ues.edu.sv>
2021-11-02 19:02:12 +0100

    Agregue una vista a consultas

---
+++ Views/Consultas/agentesnuevosyantiguos.php
@@ -0,0 +1,61 @@
+
+<?php 
+  headerAdmin($data); 
+
+?> 
+<body>
+    
+        <!--Consulta de Agentes Antiguos Y Nuevos Kelvin Alfonso Villalta Castillo VC18061 -->  
+        <?php 
+                   headerstyle1($data); 
+                   navbar1($data); 
+        ?> 
+        <!-- </div> -->
+    
+    <div style="margin-top: 90px;">
+
+        <div class="page-content">
+            <section class="row">
+                <div class="col-9 col-lg-9">
+
+                    
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header" >
+                                 <h3><i class="fas fa-allergies"></i> <?= $data['page_title'] ?>
+
+                                </h3>
+                            </div>
+                            <div class="card-body" style="margin-top: 10px;">
+                                <div class="table-responsive">
+                                    <table class="table table-striped" id="tableRoles">
+                                        <thead>
+                                            <tr>
+                                              <th>ONI</th>
+                                              <th>Nombre</th>
+                                              <th>Apellido</th>
+                                              <th>Tipo de Discapacidad</th>
+                                              <th>Acciones</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody id="datos_tabla">
+                                            
+                                        </tbody >
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+  
+                </div>
+            </section>
+        </div>
+    
+    <?php footerAdmin($data);
+          footer1($data); 
+           ?>
+
+    </div>
+    
+
+</body>
